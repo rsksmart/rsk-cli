25e8cdd9c571af0a8d3c6cf0b61f718eb13e5df9
Pratham <prathamsinghal2003@gmail.com>
2024-12-10 14:19:49 +0530

    chore: update token address

--- src/constants/tokenAdress.ts
+++ src/constants/tokenAdress.ts
@@ -3,14 +3,14 @@ import { Address } from "viem";
 export const TOKENS: Record<string, Record<string, Address>> = {
   RIF: {
     mainnet: "0x2acc95758f8b5F583470ba265eb685a8f45fc9d5",
-    testnet: "0xC370cD19517b5A8a9f6dF0958679e8cd4874C048",
+    testnet: "0x19f64674d8a5b4e652319f5e239efd3bc969a1fe",
   },
   rUSDT: {
     mainnet: "0xEf213441a85DF4d7acBdAe0Cf78004E1e486BB96",
-    testnet: "0x31974a4970BAda0ca9bCdE2e2EE6FC15922c5334",
+    testnet: "0x2694785f9c3006edf88df3a66ba7adf106dbd6a0",
   },
   rDoc: {
     mainnet: "0x2d919f19D4892381d58EdEbEcA66D5642ceF1A1F",
-    testnet: "0x7fb303D9806a72563C46aAd8D874B301419c374b",
+    testnet: "0x7fb303d9806a72563c46aad8d874b301419c374b",
   },
 };
